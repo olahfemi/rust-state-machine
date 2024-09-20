00d69164fd44c61cc21240500b47f49b006e0b45
Femi Olah <olahfemi@gmail.com>
2024-09-21 00:46:29 +0100

    Tight coupling done

--- src/balances.rs
+++ src/balances.rs
@@ -1,94 +1,112 @@
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
-use crate::types::AccountId;
-use crate::types::Balance;
-use num::Zero;
-use num::CheckedAdd;
-use num::CheckedSub;
 
-// This is the Balances Module.
-// It is a simple module which keeps track of how much balance each account has in this state
-// machine.
-/* TODO: Add the derive macro to implement the `Debug` trait for `Pallet`. */
-#[derive(Debug)]
-pub struct Pallet<AccountId, Balance> {
-	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
-	balances: BTreeMap<AccountId, Balance>,
+/*
+    TODO: Combine all generic types and their trait bounds into a single `pub trait Config`.
+    When you are done, your `Pallet` can simply be defined with `Pallet<T: Config>`.
+*/
+
+pub trait Config: crate::system::Config {
+	type Balance: Zero + CheckedSub + CheckedAdd + Copy;
 }
 
-impl<AccountId, Balance> Pallet<AccountId, Balance>
-where
-    AccountId: Ord + Clone,
-    Balance: Zero + CheckedSub + CheckedAdd + Copy, {
-	// Create a new instance of the balances module.
-	pub fn new() -> Self {
-		Self { balances: BTreeMap::new() }
-	}
+/// This is the Balances Module.
+/// It is a simple module which keeps track of how much balance each account has in this state
+/// machine.
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+    // A simple storage mapping from accounts to their balances.
+    balances: BTreeMap<T::AccountId, T::Balance>,
+}
 
-	// Set the balance of an account `who` to some `amount`.
-	pub fn set_balance(&mut self, who: &AccountId, amount: Balance) {
-		self.balances.insert(who.clone(), amount);
-	}
+/*
+    TODO: Update all of these functions to use your new configuration trait.
+*/
 
-	// Get the balance of an account `who`.
-	// If the account has no stored balance, we return zero.
-	pub fn balance(&self, who: &AccountId) -> Balance {
-		*self.balances.get(who).unwrap_or(&Balance::zero())
-	}
+impl<T: Config> Pallet<T>{
+    /// Create a new instance of the balances module.
+    pub fn new() -> Self {
+        Self { balances: BTreeMap::new() }
+    }
 
-	pub fn transfer(
-		&mut self,
-		caller: AccountId,
-		to: AccountId,
-		amount: Balance,
-	) -> Result<(), &'static str> {
-		// Get the balance of account `caller`.
-		let caller_balance = self.balance(&caller);
-		// Get the balance of account `to`.
-		let to_balance = self.balance(&to);
+    /// Set the balance of an account `who` to some `amount`.
+    pub fn set_balance(&mut self, who: &T::AccountId, amount: T::Balance) {
+        self.balances.insert(who.clone(), amount);
+    }
 
-		// Use safe math to calculate a `new_caller_balance`.
-		let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
-		// Use safe math to calculate a `new_to_balance`.
-		let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow")?;
+    /// Get the balance of an account `who`.
+    /// If the account has no stored balance, we return zero.
+    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
+        *self.balances.get(who).unwrap_or(&T::Balance::zero())
+    }
 
-		// Insert the new balance of `caller`.
-		self.balances.insert(caller, new_caller_balance);
-		// Insert the new balance of `to`.
-		self.balances.insert(to, new_to_balance);
+    /// Transfer `amount` from one account to another.
+    /// This function verifies that `from` has at least `amount` balance to transfer,
+    /// and that no mathematical overflows occur.
+    pub fn transfer(
+        &mut self,
+        caller: T::AccountId,
+        to: T::AccountId,
+        amount: T::Balance,
+    ) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
 
-		Ok(())
-	}
+        let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
+        let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow")?;
+
+        self.balances.insert(caller, new_caller_balance);
+        self.balances.insert(to, new_to_balance);
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	#[test]
-	fn init_balances() {
-		let mut balances = super::Pallet::<&'static str, u128>::new();
-
-
-		assert_eq!(balances.balance(&"alice".to_string()), 0);
-		balances.set_balance(&"alice".to_string(), 100);
-		assert_eq!(balances.balance(&"alice".to_string()), 100);
-		assert_eq!(balances.balance(&"bob".to_string()), 0);
+    /*
+        TODO: Create a `struct TestConfig`, and implement `super::Config` on it with concrete types.
+        Use this struct to instantiate your `Pallet`.
+    */
+	struct TestConfig;
+	impl super::Config for TestConfig {
+		type AccountId = String;
+		type Balance = u32;
 	}
 
-	#[test]
-	fn transfer_balance() {
-		let mut balances = super::Pallet::new();
-
-		assert_eq!(
-			balances.transfer("alice".to_string(), "bob".to_string(), 10),
-			Err("Not enough funds.")
-		);
-
-		balances.set_balance(&"alice".to_string(), 10);
-		assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 10), Ok(()));
-		assert_eq!(balances.balance(&"alice".to_string()), 0);
-		assert_eq!(balances.balance(&"bob".to_string()), 10);
-		assert_eq!(
-			balances.transfer("alice".to_string(), "bob".to_string(), 10),
-			Err("Not enough funds.")
-		);
+	impl crate::system::Config for TestConfig {
+		type AccountId = String;
+        type BlockNumber = u32;
+        type Nonce = u32;
 	}
+
+    #[test]
+    fn init_balances() {
+        let mut balances = super::Pallet::<TestConfig>::new();
+
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        assert_eq!(balances.balance(&"bob".to_string()), 0);
+    }
+
+    #[test]
+    fn transfer_balance() {
+        let mut balances = super::Pallet::<TestConfig>::new();
+
+        assert_eq!(
+            balances.transfer("alice".to_string(), "bob".to_string(), 51),
+            Err("Not enough funds.")
+        );
+
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 51), Ok(()));
+        assert_eq!(balances.balance(&"alice".to_string()), 49);
+        assert_eq!(balances.balance(&"bob".to_string()), 51);
+
+        assert_eq!(
+            balances.transfer("alice".to_string(), "bob".to_string(), 51),
+            Err("Not enough funds.")
+        );
+    }
 }
