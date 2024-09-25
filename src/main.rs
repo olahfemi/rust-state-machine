c83675177e2d24f9573e74facf6616fb2bccba63
Femi Olah <olahfemi@gmail.com>
2024-09-26 00:40:18 +0100

    Execute block function built

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod balances;
 mod support;
 mod system;
 
+/* TODO: Import `crate::support::Dispatch` so that you can access the `dispatch` function. */
+use crate::support::Dispatch;
+
 // These are the concrete types we will use in our simple state machine.
 // Modules are configured for these types directly, and they satisfy all of our
 // trait requirements.
@@ -10,12 +13,9 @@ mod types {
     pub type Balance = u128;
     pub type BlockNumber = u32;
     pub type Nonce = u32;
-    /* TODO: Define a concrete `Extrinsic` type using `AccountId` and `RuntimeCall`. */
-	pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
-    /* TODO: Define a concrete `Header` type using `BlockNumber`. */
-	pub type Header = crate::support::Header<BlockNumber>;
-    /* TODO: Define a concrete `Block` type using `Header` and `Extrinsic`. */
-	pub type Block = crate::support::Block<Header, Extrinsic>;
+    pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
+    pub type Header = crate::support::Header<BlockNumber>;
+    pub type Block = crate::support::Block<Header, Extrinsic>;
 }
 
 // These are all the calls which are exposed to the world.
@@ -46,6 +46,57 @@ impl Runtime {
     // Create a new instance of the main Runtime, by creating a new instance of each pallet.
     fn new() -> Self {
         Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+    }
+
+    // Execute a block of extrinsics. Increments the block number.
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        /* TODO:
+            - Increment the system's block number.
+            - Check that the block number of the incoming block matches the current block number,
+              or return an error.
+            - Iterate over the extrinsics in the block...
+                - Increment the nonce of the caller.
+                - Dispatch the extrinsic using the `caller` and the `call` contained in the extrinsic.
+                - Handle errors from `dispatch` same as we did for individual calls: printing any
+                  error and capturing the result.
+                - You can extend the error message to include information like the block number and
+                  extrinsic number.
+        */
+        self.system.inc_block_number();
+        if block.header.block_number != self.system.block_number() {
+            return Err("block number does not match what is expected")
+        }
+
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            // do stuff with `caller` and `call`
+            self.system.inc_nonce(&caller);
+            let _res = self.dispatch(caller, call).map_err(|e| {
+                eprintln!(
+                    "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                    block.header.block_number, i, e
+                )
+            });
+        }
+
+        
+        Ok(())
+    }
+}
+
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+    // Dispatch a call on behalf of a caller. Increments the caller's nonce.
+    //
+    // Dispatch allows us to identify which underlying module call we want to execute.
+    // Note that we extract the `caller` from the extrinsic, and use that information
+    // to determine who we are executing the call on behalf of.
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+        unimplemented!();
     }
 }
 
