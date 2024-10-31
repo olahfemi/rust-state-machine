69fd8da229f11cdc6386adcb975c22639c5976f4
Femi Olah <olahfemi@gmail.com>
2024-10-31 17:36:03 +0100

    Add Proof of Existence Dispatch

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -26,19 +26,15 @@ impl<T: Config> Pallet<T> {
 
     /// Get the owner (if any) of a claim.
     pub fn get_claim(&self, claim: &T::Content) -> Option<&T::AccountId> {
-        /* TODO: `get` the `claim` */
         self.claims.get(claim)
     }
 
     /// Create a new claim on behalf of the `caller`.
     /// This function will return an error if someone already has claimed that content.
     pub fn create_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
-        /* TODO: Check that a `claim` does not already exist. If so, return an error. */
         if self.claims.contains_key(&claim) {
-            return Err(&"this content is already claimed");
+            return Err("this content is already claimed");
         }
-        
-        /* TODO: `insert` the claim on behalf of `caller`. */
         self.claims.insert(claim, caller);
         Ok(())
     }
@@ -47,14 +43,42 @@ impl<T: Config> Pallet<T> {
     /// This function should only succeed if the caller is the owner of an existing claim.
     /// It will return an error if the claim does not exist, or if the caller is not the owner.
     pub fn revoke_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
-        /* TODO: Get the owner of the `claim` to be revoked. */
-       let owner = self.get_claim(&claim).ok_or("claim does not exist")?;
-        /* TODO: Check that the `owner` matches the `caller`. */
-        /* TODO: If all checks pass, then `remove` the `claim`. */
+        let owner = self.get_claim(&claim).ok_or("claim does not exist")?;
         if caller != *owner {
             return Err("this content is owned by someone else");
         }
         self.claims.remove(&claim);
+        Ok(())
+    }
+}
+
+// A public enum which describes the calls we want to expose to the dispatcher.
+// We should expect that the caller of each call will be provided by the dispatcher,
+// and not included as a parameter of the call.
+pub enum Call<T: Config> {
+    CreateClaim { claim: T::Content },
+    RevokeClaim { claim: T::Content },
+}
+
+/// Implementation of the dispatch logic, mapping from `POECall` to the appropriate underlying
+/// function we want to execute.
+impl<T: Config> crate::support::Dispatch for Pallet<T> {
+    type Caller = T::AccountId;
+    type Call = Call<T>;
+
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        call: Self::Call,
+    ) -> crate::support::DispatchResult {
+        match call {
+            Call::CreateClaim { claim } => {
+                self.create_claim(caller, claim)?;
+            },
+            Call::RevokeClaim { claim } => {
+                self.revoke_claim(caller, claim)?;
+            },
+        }
         Ok(())
     }
 }
@@ -75,18 +99,15 @@ mod test {
 
     #[test]
     fn basic_proof_of_existence() {
-
-        let mut claims = super::Pallet::<TestConfig>::new();
-
-        assert_eq!(claims.get_claim(&"Alice"), None);
-        assert_eq!(claims.create_claim(&"Alice", "Hello, world!"), Ok(()));
-        assert_eq!(claims.get_claim(&"Hello, world!"), Some(&"Alice"));
+        let mut poe = super::Pallet::<TestConfig>::new();
+        assert_eq!(poe.get_claim(&"Hello, world!"), None);
+        assert_eq!(poe.create_claim("alice", "Hello, world!"), Ok(()));
+        assert_eq!(poe.get_claim(&"Hello, world!"), Some(&"alice"));
         assert_eq!(
-            claims.create_claim("Bob", "Hello, world!"),
+            poe.create_claim("bob", "Hello, world!"),
             Err("this content is already claimed")
         );
-        assert_eq!(claims.revoke_claim("Alice", "Hello, world!"), Ok(()));
-        assert_eq!(claims.create_claim("Bob", "Hello, world!"), Ok(()));
+        assert_eq!(poe.revoke_claim("alice", "Hello, world!"), Ok(()));
+        assert_eq!(poe.create_claim("bob", "Hello, world!"), Ok(()));
     }
-
 }
